9e56967abd20f2604b7a2ee1d6d2a3cdaa5df3bb
Olegzandr Denman <alex@oresoftware.com>
2018-06-24 18:30:10 -0700

    pdev:'set'

---
+++ scripts/npm/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+cmd="npm install $@";
+echo "$cmd" | bash;
+
+if [ "$?" != "0" ]; then
+  echo "Could not run npm install command: $cmd";
+  exit 1;
+fi
+
+if ! command -v nlu &> /dev/null; then
+   npm i -g -s 'npm-link-up' || {
+     echo "Could not install npm-link-up";
+     echo "Check your permissions to install global NPM packages.";
+     exit 1;
+   }
+fi
+
+echo "Now running npm-link-up...";
+nlu run;
