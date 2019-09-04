d4023b688c8074408dccc0b2b6779af5b52bc4ac
Alexander Mills <alex@alexs-mac.local>
2019-09-04 12:34:12 -0700

    tmp

---
+++ scripts/test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+set -e;
+
+cd  "$(dirname "$(dirname "$BASH_SOURCE")")"
+
+cd test
+tsc -p tsconfig.test.json
+
+for f in `(cd 'dist' && find . -type f)`; do
+  node "dist/$f"
+done
+
+echo 'All tests exited with 0.'
+echo 'Success'
+
+exit 0;
+
+
+
